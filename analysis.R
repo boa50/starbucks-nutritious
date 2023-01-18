ccfd71731096fda60a5319b1f1c4ca17f9a868b8
boa50 <bruno.albuquerque50@gmail.com>
2023-01-18 19:05:34 -0300

    Replaced NA values in the dataframe

--- analysis.R
+++ analysis.R
@@ -8,9 +8,18 @@ library(stringr)
 
 df <- read_csv("dataset/DataDNA Dataset Challenge -- January 2023.csv")
 
+
+### Getting rid of NA values
+colSums(is.na(df))
+
+# Setting with 0, because if we don't have the information, we expect that
+# there isn't any quantity of the nutrient
+df <- mutate_all(df, ~ifelse(is.na(.), 0, .))
+
+
 ### Setting a column with coffees' sizes.
-available_sizes <- c("Short", "Tall", "Grande", "Venti")
-available_expresso_sizes <- c("Solo", "Doppio")
+# available_sizes <- c("Short", "Tall", "Grande", "Venti")
+# available_expresso_sizes <- c("Solo", "Doppio")
 
 df$sizes <- str_extract(df$Beverage_prep, "^(Short|Tall|Grande|Venti|Solo|Doppio)")
 df <- df %>% 
