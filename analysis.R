71f27cb1b64dbc96827e76908fa7ca33ab21f418
boa50 <bruno.albuquerque50@gmail.com>
2023-01-18 18:54:51 -0300

    Created a column in the dataframe with information of coffees's sizes

--- analysis.R
+++ analysis.R
@@ -1,10 +1,17 @@
 library(ggplot2)
 library(readr)
 library(dplyr)
+library(tidyr)
+library(stringr)
 
 ### Question: Can you identify the most nutritious items on the menu?
 
 df <- read_csv("dataset/DataDNA Dataset Challenge -- January 2023.csv")
-head(df)
-str(df)
-colnames(df)
+
+### Setting a column with coffees' sizes.
+available_sizes <- c("Short", "Tall", "Grande", "Venti")
+available_expresso_sizes <- c("Solo", "Doppio")
+
+df$sizes <- str_extract(df$Beverage_prep, "^(Short|Tall|Grande|Venti|Solo|Doppio)")
+df <- df %>% 
+  fill(sizes, .direction = "down")
