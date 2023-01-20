7459ec89c2d1853bece5e1db4aa1083e19e57315
boa50 <bruno.albuquerque50@gmail.com>
2023-01-20 07:45:44 -0300

    Created a sample dashboard with a interactive slicer

--- analysis.R
+++ analysis.R
@@ -3,11 +3,18 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(stringr)
+library(janitor)
 
 ### Question: Can you identify the most nutritious items on the menu?
 
 df <- read_csv("dataset/DataDNA Dataset Challenge -- January 2023.csv")
+str(df)
 
+### Renaming columns
+df <- clean_names(df)
+
+### Converting caffeine column to double
+df$caffeine_mg <- as.numeric(df$caffeine_mg)
 
 ### Getting rid of NA values
 colSums(is.na(df))
@@ -16,11 +23,9 @@ colSums(is.na(df))
 # there isn't any quantity of the nutrient
 df <- mutate_all(df, ~ifelse(is.na(.), 0, .))
 
-
 ### Setting a column with coffees' sizes.
 # available_sizes <- c("Short", "Tall", "Grande", "Venti")
 # available_expresso_sizes <- c("Solo", "Doppio")
-
-df$sizes <- str_extract(df$Beverage_prep, "^(Short|Tall|Grande|Venti|Solo|Doppio)")
+df$size <- str_extract(df$beverage_prep, "^(Short|Tall|Grande|Venti|Solo|Doppio)")
 df <- df %>% 
-  fill(sizes, .direction = "down")
+  fill(size, .direction = "down")
